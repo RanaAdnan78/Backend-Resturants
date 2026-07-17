50243a8a3f1622cb80367063af85c5f4133f5dc0
Rana Adnan <adnanrana6111@gmail.com>
2026-07-17 14:44:59 +0500

    New Files

--- restauapp-api/main.py
+++ restauapp-api/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routes import auth
+
 app = FastAPI(title="RestauApp API")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 
 @app.get("/")
 async def home():
